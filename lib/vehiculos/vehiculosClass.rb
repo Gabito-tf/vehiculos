23369c19634b94979cc8cd8f985df9bca7507809
Gabito-tf <alu0101098340@ull.edu.es>
2022-10-30 16:12:41 +0000

    ACIERTO: Atributo para el año de fabricacion

--- lib/vehiculos/vehiculosClass.rb
+++ lib/vehiculos/vehiculosClass.rb
@@ -1,11 +1,19 @@
 class VehiculosClass
 
     # Getters
-    attr_reader :id_ , :fabricante_
+    attr_reader :id_ , :fabricante_, :ano_
 
     # Constructor
-    def initialize(id_,fabricante_)
+    def initialize(id_,fabricante_,ano_)
+        # Atributos
         @id_ , @fabricante_ = id_, fabricante_
+        # Comprobación de que el año es un valor entre 1900 y 2022.
+        if ano_ >= 1900 && ano_ <= 2022
+            @ano_ = ano_
+        else
+            raise ArgumentError, "El año debe estar entre 1900 y 2022."
+        end
+        
     end
 
 end
