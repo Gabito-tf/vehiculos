359b8d4a5a5ee73b06e5bdc94f96e4da3a230b09
Gabito-tf <alu0101098340@ull.edu.es>
2022-10-30 16:02:51 +0000

    FALLO: Atributo para el año de fabricacion

--- spec/vehiculosClass_spec.rb
+++ spec/vehiculosClass_spec.rb
@@ -3,7 +3,7 @@ require 'vehiculos/vehiculosClass'
 RSpec.describe VehiculosClass do
   describe "Pruebas de la clase Vehiculos" do
     before :each do
-      @vehiculo1 = VehiculosClass.new("2933LBL","KIA")
+      @vehiculo1 = VehiculosClass.new("2933LBL","KIA",2019)
     end
 
     it "Debe existir la clase vahículos" do
@@ -11,13 +11,24 @@ RSpec.describe VehiculosClass do
     end
 
     it "Debe existir un atributo para identificar el vehículo" do
+      #Expectativa para comprobar que el atributo id_ no es nulo."
       expect(@vehiculo1.id_).not_to be nil
+      #Expectativa para comprobar que el atributo id_ es una matrícula."
       expect(@vehiculo1.id_).to eq("2933LBL")
     end
 
     it "Debe existir un atributo para el fabricante del vehículo" do
+      #Expectativa para comprobar que el fabricante no es nulo.
       expect(@vehiculo1.fabricante_).not_to be nil
+      #Expectativa para comprobar que el fabricante es KIA.
       expect(@vehiculo1.fabricante_).to eq("KIA")
+    end
+
+    it "Debe existir un atributo para el año de fabricación del vehículo" do
+      #Expectativa para comprabar que el año de fabricación no es nulo.
+      expect(@vehiculo1.ano_).not_to be nil
+      #Expectativa para comprobar si el año es mayor que 1900 y menor que 2022.
+      expect(@vehiculo1.ano_).to be_between(1900,2022)
     end
 
   end
