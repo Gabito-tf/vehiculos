02cb0d00215b391f3a2f8e76e95d7da313a2d32e
Gabito-tf <alu0101098340@ull.edu.es>
2022-10-30 15:49:13 +0000

    ACIERTO: Atributo indentificador de vehículo

--- lib/vehiculos/vehiculosClass.rb
+++ lib/vehiculos/vehiculosClass.rb
@@ -1,2 +1,9 @@
 class VehiculosClass
+
+    attr_reader :id_
+
+    def initialize(id_)
+        @id_ = id_
+    end
+
 end
--- spec/vehiculosClass_spec.rb
+++ spec/vehiculosClass_spec.rb
@@ -3,7 +3,7 @@ require 'vehiculos/vehiculosClass'
 RSpec.describe VehiculosClass do
   describe "Pruebas de la clase Vehiculos" do
     before :each do
-      @vehiculo1 = VehiculosClass.new("3933LBL")
+      @vehiculo1 = VehiculosClass.new("2933LBL")
     end
 
     it "Debe existir la clase vahículos" do
@@ -11,7 +11,8 @@ RSpec.describe VehiculosClass do
     end
 
     it "Debe existir un atributo para identificar el vehículo" do
-      expect(@vehiculo1).to respond_to(:id)
+      expect(@vehiculo1.id_).not_to be nil
+      expect(@vehiculo1.id_).to eq("2933LBL")
     end
 
   end
