4a4e2d97b3677f80a1645de5e0bbcddbbb5a6560
Gabito-tf <alu0101098340@ull.edu.es>
2022-10-30 15:44:28 +0000

    FALLO: Atributo indentificador de vehículo

--- spec/vehiculosClass_spec.rb
+++ spec/vehiculosClass_spec.rb
@@ -3,11 +3,17 @@ require 'vehiculos/vehiculosClass'
 RSpec.describe VehiculosClass do
   describe "Pruebas de la clase Vehiculos" do
     before :each do
-      @vehiculo1 = VehiculosClass.new()
+      @vehiculo1 = VehiculosClass.new("3933LBL")
     end
 
     it "Debe existir la clase vahículos" do
       expect(@vehiculo1).to be_an_instance_of(VehiculosClass)
     end
+
+    it "Debe existir un atributo para identificar el vehículo" do
+      expect(@vehiculo1).to respond_to(:id)
+    end
+
   end
+
 end
