8a0404a080027393e3246f4926b8213c0a9bcf7d
Gabito-tf <alu0101098340@ull.edu.es>
2022-10-30 15:31:56 +0000

    Fallo: Existe la clase

---
+++ spec/vehiculosClass_spec.rb
@@ -0,0 +1,7 @@
+require 'vehiculos/vehiculosClass'
+
+RSpec.describe Vehiculos do
+  describe "Pruebas de la clase Vehiculos" do
+    do ec
+  end
+end
--- spec/vehiculos_spec.rb
+++ spec/vehiculos_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe Vehiculos do
   it "has a version number" do
     expect(Vehiculos::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
